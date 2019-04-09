2ac4fb7ec947e03d6397613d76926d417c3f8873
Allan Wang <wang.allan@outlook.com>
2019-04-09 17:22:43 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+//const chalk = require('chalk');
+const yargs = require('yargs');
+//const getNotes = require('./notes');
+
+// Customize yargs version
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true, // force this option as required
+      type: 'string'
+    },
+    body: {
+      describe: 'Note Body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    console.log('Title:', argv.title);
+    console.log('Body:', argv.body);
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler() {
+    console.log('Remove a note!');
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler() {
+    console.log('Listing out all notes!');
+  }
+});
+
+// Reading command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler() {
+    console.log('Reading a note!');
+  }
+});
+
+yargs.parse();
